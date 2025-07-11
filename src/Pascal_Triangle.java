f062348dc4722c08d0e30089d98626f8015c74b7
shothp345@gmail.com <shothp345@gmail.com>
2025-07-11 10:56:34 +0530

    first commit

---
+++ src/Pascal_Triangle.java
@@ -0,0 +1,16 @@
+public class Pascal_Triangle {
+    public static void main(String[] args) {
+        int row=5;
+        for(int i=0; i<row; i++){
+            for(int j=0; j<row-i; j++){
+                System.out.print(" ");
+            }
+            int number =1;
+            for(int k=0; k<=i;k++){
+                System.out.print(number+" ");
+                number=number*(i-k)/(k+1);
+            }System.out.println();
+        }
+
+    }
+}
